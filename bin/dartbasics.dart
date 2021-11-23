ab6618cc0bad85f5aefbc2b7223799c5c7f59116
ella-tuglo <ellatuglo@gmail.com>
2021-11-23 18:15:23 +0000

    first commit

---
+++ bin/dartbasics.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+void main() {
+  print('hello world');
+  var barrel = 'water';
+  var bottle = 'oil';
+  var brokenheart = 'alex';
+  var jahbless = 'Godbless';
+  print(brokenheart);
+  print(brokenheart);
+
+  var num1 = '2999';
+  var num2 = '457';
+  var num3 = '2345';
+  var num4 = '678';
+  var num5 = '4560';
+  var num = num1 + num2 + num3 + num4 + num5;
+  print(num);
+
+  var string = 'i hate monday class';
+  var string2 = '$string but i love progrramming';
+  print(string);
+
+  var myage = 19;
+  var mynameandage = 'my name is ella and i am $myage years old';
+  print(myage);
+
+  List fruit = [
+    'apple',
+    'banana',
+    'orange',
+    'pinapple',
+  ];
+  List classmembers = ['ella', 'tyh', 'hybn', 'hujj'];
+  print(fruit);
+  print(fruit.length);
+  print(fruit[2]);
+  print(classmembers);
+}
